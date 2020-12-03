7557bc7f436970fc831f973e8882efa3f4b947b8
Kevin Xiao <xlzqwerty1@gmail.com>
2020-12-02 20:05:28 -0500

    Add Netlify badge to footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,6 +55,13 @@ module.exports = {
               label: 'Discord',
               href: 'https://discord.gg/NxGAeCY',
             },
+            {
+              html: `
+              <a href="https://www.netlify.com">
+                <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg" alt="Deploys by Netlify" />
+              </a>
+              `
+            }
           ],
         },
         {
