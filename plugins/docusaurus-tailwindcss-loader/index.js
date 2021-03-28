8d387c5c10b758c015a705e9f2c7e95dc219ad7b
Kevin Xiao <xlzqwerty1@gmail.com>
2021-03-28 19:24:13 -0400

    Upgrade docusaurus to alpha.72

--- plugins/docusaurus-tailwindcss-loader/index.js
+++ plugins/docusaurus-tailwindcss-loader/index.js
@@ -1,34 +1,19 @@
 module.exports = function (context, options) {
   return {
     name: 'postcss-tailwindcss-loader',
-    configureWebpack(config, isServer, utils) {
-      return {
-        module: {
-          rules: [
-            {
-              test: /\.css$/,
-              use: [
-                {
-                  loader: require.resolve('postcss-loader'),
-                  options: {
-                    ident: 'postcss',
-                    plugins: () => [
-                      require('postcss-import'),
-                      require('tailwindcss'),
-                      require('postcss-preset-env')({
-                        autoprefixer: {
-                          flexbox: 'no-2009',
-                        },
-                        stage: 4,
-                      }),
-                    ],
-                  },
-                },
-              ],
-            },
-          ],
-        },
-      }
+    configurePostCss(postcssOptions) {
+      // Appends new PostCSS plugin.
+      postcssOptions.plugins.push(
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('postcss-preset-env')({
+          autoprefixer: {
+            flexbox: 'no-2009',
+          },
+          stage: 4,
+        }),
+      )
+      return postcssOptions
     },
   }
 }
