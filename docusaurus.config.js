163c664fe7eba8cfabea5b3119a0093f0202edcd
Doc <nachito94@msn.com>
2026-04-13 11:37:28 -0400

    fix: update netlify badge link assets

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -85,13 +85,13 @@ module.exports = {
                         },
                         {
                             html: `
-              <a href="https://www.netlify.com">
-                <img
-                  src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg"
-                  alt="Deploys by Netlify"
-                />
-              </a>
-              `,
+                                  <a href="https://www.netlify.com">
+                                    <img
+                                      src="https://www.netlify.com/assets/badges/netlify-badge-color-accent.svg"
+                                      alt="Deploys by Netlify"
+                                    />
+                                  </a>
+                                  `,
                         },
                     ],
                 },
